7549608c44353bc8467454d75838a3f1581fca24
kaktuz <kaktuz911@gmail.com>
2023-04-30 13:28:00 +0300

    update package.json and 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryRef = document.querySelector('.gallery');
+const galleryCard = createGalleryCard(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryCard);
+
+
+function createGalleryCard(gallery) {
+    return gallery.map(({ description, preview, original }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}"">
+            <img 
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}">
+        </a>
+    </li>
+    `;
+    }).join('');
+     
+}
+console.log(galleryCard)
+
+    const lightbox = new SimpleLightbox('.gallery__item a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+
